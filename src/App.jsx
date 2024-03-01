2eac633193e81a408be8b487989ef18cf83d820f
francksefu <francksefu1998@gmail.com>
2024-03-01 12:25:30 +0200

    finish with experience part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import GeneralInformation from './components/GeneralInformation'
 import Education from './components/Education'
+import Experience from './components/Experience'
 
 function App() {
   return (
     <>
       <GeneralInformation />
       <Education />
+      <Experience />
     </>
   )
 }
